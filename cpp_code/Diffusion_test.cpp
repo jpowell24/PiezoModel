8d3e4a2c9379c85b81ace5e8b062acf6f15f9c06
jpowell24 <jrp24@sas.upenn.edu>
2023-06-02 15:26:48 -0400

    backward eulers try 1
    note that I did indeed break everything else, thank you.

--- cpp_code/Diffusion_test.cpp
+++ cpp_code/Diffusion_test.cpp
@@ -1,32 +1,105 @@
-// #define ARMA_DONT_USE_WRAPPER
-#include <armadillo>
+#include <Eigen/Dense>
 #include <iostream>
+#include <cmath>
+#include <fstream>
+#include <sstream>
+#include <string.h>
+#include <vector>
+#include <algorithm>
+#include <random>
 
-//#include <C:/Users/Jackson/Code/PiezoModel/cpp_code/lapack-3.11/OPENBlas>
-
-
-// NOTE: I went into the file def_lapack.hpp and commented 
-// out a bunch of things that almost certainly destroy the code
-// ctrl+f for "//" to find which
 
 using namespace std;
-using namespace arma;
+using namespace Eigen;
 
-int main()
-{
-    //initialize the random generator 
-    //Create a 5 x 5 random matrix and printing it
+const char *path1="../data_files/Diffusion_test.csv";
+
+
+int main(){
+	cout << "begin" << endl;
+	ofstream create_file(path1);
+    ofstream myfile;
+    myfile.open(path1);
+
+	double delta_t = 0.01/1000; //in seconds
+	double delta_x = 0.1/1000; //in meters
+
+	double total_t = 0.0001;
+	double time_index = total_t/delta_t;
+
+	double initial_C = 0.1;
+
+	int size = 20; 
+
+	for(double time = 0; time < time_index; time ++){
+
 	
-	mat A = randu<mat>(5,5); //random matrix of size 5x5 (generared by this syntax)
-	mat B = inv(A);	         //inverse of matrix
-    
-	cout << "Matrix A::\n"<<endl;
-    cout<< A << endl;
-	cout << "Matrix B(Inverse)::\n"<<endl;
-  	
-	cout << B << endl;
+	MatrixXd A = MatrixXd::Zero(size, size);
+	
+	// cout << "Here is the matrix A:\n" << A << "\n" << endl;
 
-	cout << inv(A) << endl;
+	for(int i = 0; i < size; i++){
+		for(int j = 0; j < size; j++){
+			if((i < size - 1) && (j < size - 1) && (i > 0) && (j > 0)){
+				if(i == j){
+					A(i,j) = -2;
+					A(i,j + 1) = 1;
+					A(i,j - 1) = 1;
+				}
+			}
+			if((i == 0)){
+				if(i == j){
+					A(i,j) = -1;
+					A(i,j + 1) = 1;
+				}
+			}
+			if((i == size - 1)){
+				if(i == j){
+					A(i,j) = -1;
+					A(i,j - 1) = 1;
+				}
+			}
+		}
+	}
 
-	return 0;
+	A = (delta_t / pow(delta_x,2))*A;
+
+	// cout << A << endl;
+
+	VectorXd F1(size);
+	for(int i = 0; i < size; i++){
+		F1[i] = initial_C;
+	}
+	F1[5] = 10;
+
+
+	VectorXd F2 = VectorXd::Zero(size);
+
+	// cout << F1 << " and " << endl;
+
+	// cout << F1 << "\n" << endl;
+
+	MatrixXd I(size, size);
+	I.setIdentity(size, size);
+
+	F2 = (I - A).inverse()*F1;
+
+	// cout << F2 << endl;
+
+	for (int i = 0; i < size - 1; i++)
+    {
+        if(time < time_index - 1) myfile << F2[i] << ",";
+        if(time == time_index - 1) myfile << F2[i] << ",";
+    }
+
+	myfile << "\n";
+
+	F1 = F2; 
+	cout << time << endl;
+	}
+
+	myfile.close();
+	cout << "end" << endl;
+
+	return(0);
 }
